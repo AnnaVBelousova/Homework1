41565bde1c9711968d0d6aadf17a4adf197ab25e
anna <a1b2v3@mail.ru>
2022-12-03 15:43:51 +0500

    добавили ex1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Семинар1С_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int max = a;
+            
+            if(b > max)
+            {
+            max = b;
+            }
+
+            if(c > max)
+            {
+            max = c;
+            }
+            Console.WriteLine(max);
         }
     }
 }
